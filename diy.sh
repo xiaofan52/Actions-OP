971ec0d2cd92eb86398683fdbfa76c958b8051d5
xiaofan52 <46153056+xiaofan52@users.noreply.github.com>
2021-05-19 13:28:08 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,6 +5,6 @@
 # Author: digitcloud
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.222/g' package/base-files/files/bin/config_generate
 
 
